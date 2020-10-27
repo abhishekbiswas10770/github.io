212300b74b0a1215db366615716c57a087fc737a
abhishekbiswas10770 <69751840+abhishekbiswas10770@users.noreply.github.com>
2020-10-27 13:26:24 +0530

    First Game

---
+++ index.js
@@ -0,0 +1,39 @@
+function game(a) {
+    var computer = Math.floor(Math.random() * (4 - 1) + 1);
+
+
+    if (computer <= 1) {
+        document.getElementById("output").innerHTML = "<img src='./images/rock.jpeg'>"
+
+    } else if (computer <= 2) {
+        document.getElementById("output").innerHTML = "<img src='./images/paper.jpeg'>"
+    } else {
+        document.getElementById("output").innerHTML = "<img src='./images/scissors.jpeg'>"
+    }
+
+    const ans = (a, computer) => {
+        if (a == 1 && computer == 2) {
+            document.getElementById("result").innerHTML = "<h1 >YOU LOOSE</h1>"
+        }
+        if (a == 1 && computer == 3) {
+            document.getElementById("result").innerHTML = "<h1>YOU WIN</h1>";
+        }
+        if (a == 2 && computer == 1) {
+            document.getElementById("result").innerHTML = "<h1>YOU WIN</h1>";
+        }
+        if (a == 2 && computer == 3) {
+            document.getElementById("result").innerHTML = "<h1>YOU LOOSE</h1>";
+        }
+        if (a == 3 && computer == 1) {
+            document.getElementById("result").innerHTML = "<h1>YOU LOOSE</h1>";
+        }
+        if (a == 3 && computer == 2) {
+            document.getElementById("result").innerHTML = "<h1>YOU WIN</h1>";
+        }
+
+    }
+    ans(a, computer);
+    if (a == computer) { document.getElementById("result").innerHTML = "<h1>TIE</h1>" }
+
+
+}
